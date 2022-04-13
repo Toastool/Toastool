533ad6fe17516fae5ee707c0bcec79d7227e8ac9
Bang57 <90671160+Bang57@users.noreply.github.com>
2022-04-13 13:21:56 +0900

    first

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        if(inputX != 0 || inputY != 0)
+            anim.SetBool("ismove", true);
+        else
+            anim.SetBool("ismove", false);
+
+        anim.SetFloat("inputx", inputX);
+        anim.SetFloat("inputy", inputY);
+
+        transform.Translate(moveSpeed * Time.deltaTime * new Vector2(inputX, inputY));
+
+        Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position); //캐릭터의 월드 좌표를 뷰포트 좌표계로 변환해준다.
+        viewPos.x = Mathf.Clamp01(viewPos.x); //x값을 0이상, 1이하로 제한한다.
+        viewPos.y = Mathf.Clamp01(viewPos.y); //y값을 0이상, 1이하로 제한한다.
+        Vector3 worldPos = Camera.main.ViewportToWorldPoint(viewPos); //다시 월드 좌표로 변환한다.
+        transform.position = worldPos; //좌표를 적용한다.
+    }
+}
